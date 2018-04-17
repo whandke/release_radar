7403fbbb372f1245a732f667ffc9ca648bb90eb5
whandke <handkewojciech@gmail.com>
2018-04-18 01:00:08 +0200

    Giant midweek commit
    Added:
    > add.ejs page and handling for adding new docs to DB
    > games.ejs page and displaying docs from DB
    > handling for search bar on index page throught get method
    > css rules for new form on Add page
    > css rules for new Game List
    
    Changed:
    > little tweaks in css for overall better looks

--- controllers/radarController.js
+++ controllers/radarController.js
@@ -4,38 +4,63 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://app:test@ds129926.mlab.com:29926/radar');
 
 let gameSchema = new mongoose.Schema({
-  name: String,
-  year: String
+  title: String,
+  studio: String,
+  publisher: String,
+  date: String
 })
 
-let Game = mongoose.Model('Game', gameSchema);
+let Games = mongoose.model('Game', gameSchema);
 
 module.exports = function(app){
 
   app.get('/', function(req, res){
       res.render('index');
-    })
+  })
       
   app.get('/home', function(req, res){
       res.redirect('/');
+  })
+
+  app.get('/games', function(req, res){
+    if(req.query.search){
+      Games.find({title: new RegExp(req.query.search, "i")}, function(err, data){
+        if(err) throw err;
+        res.render('games', {games: data});
+      })
+    }
+    else {
+      Games.find({}, function(err, data){
+        if(err) throw err;
+        res.render('games', {games: data});
+      })
+    }
+    
+  })
+
+  app.get('/add', function(req, res){
+    res.render('add');
+  })
+
+  app.post('/add', function(req, res){
+    let newGame = Games(req.body).save(function(err, data){
+      if(err) throw err;
+      res.render('games');
     })
+  })
       
   app.get('/contact', function(req, res){
-      res.render('contact');
-    })
-      
-  app.post('/search', function(req, res){
-      res.render('search', {data: req.body});
-    })
+    res.render('contact');
+  })
 
   app.get('/about', function(req, res){
-      res.render('about');
-    })
+    res.render('about');
+  })
       
   app.get('*', function(req, res){
-      console.log('Unhandled request was made: ' + req.path);
-      console.log('Sending 404 page...');
-      res.render('404');
-    })
+    console.log('Unhandled request was made: ' + req.path);
+    console.log('Sending 404 page...');
+    res.render('404');
+  })
 
 }
